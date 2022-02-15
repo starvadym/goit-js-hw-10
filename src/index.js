6f6c3a1d38a681d6c93e406081de00bc8f945877
Vadym Starchenko <starvadim@ukr.net>
2022-02-15 08:41:20 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -31,7 +31,7 @@ function onSuccess(data) {
       updateCountries();
       createCountryInfo(data);
     return;
-  }
+    }
     if (data.length > 10) {
     updateCountries();
     Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
@@ -44,26 +44,27 @@ function onSuccess(data) {
 function onError() {
   updateCountries();
   Notiflix.Notify.failure('Oops, there is no country with that name');
- }
+}
 
 function createCountryInfo (data) {
- const markup = data.map(country => {
+   const markup = data.map(country => {
         return `<img src="${country.flags.svg}" alt="Flag" width="30" height="24"></img>
                 <h2 class="country-info-title">${country.name.official}</h2>
             <p>Capital: <span>${country.capital}</span></p>
             <p>Population: <span>${country.population}</span></p>
             <p>Languages: <span>${Object.values(country.languages)}</span></p>`
- })
-     .join('');
+   })
+    .join('');
     refsInfo.insertAdjacentHTML('beforeend', markup);
 }
 
 function createCountryList (data) {
-    const markup = data.map(country => {
-        return `<li class="country-list-item">
+   const markup = data.map(country => {
+       return `<li class="country-list-item">
         <img class="flag" src="${country.flags.svg}" alt="Flag" width="20" height="16"></img>
         ${country.name.official}</li>`
-    }).join('');
+   })
+    .join('');
     refsList.insertAdjacentHTML('beforeend', markup);
 }
 

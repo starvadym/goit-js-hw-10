f07e3d9a646e07acfde5fc6f963c93dfb093b471
Vadym Starchenko <starvadim@ukr.net>
2022-02-15 09:07:27 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -28,9 +28,9 @@ function onInputHandle(e) {
 function onSuccess(data) {
     console.log(data.length);
     if (data.length === 1) {
-      updateCountries();
-      createCountryInfo(data);
-    return;
+        updateCountries();
+        createCountryInfo(data);
+     return;
     }
     if (data.length > 10) {
     updateCountries();
@@ -46,13 +46,13 @@ function onError() {
   Notiflix.Notify.failure('Oops, there is no country with that name');
 }
 
-function createCountryInfo (data) {
+function createCountryInfo(data) {
    const markup = data.map(country => {
         return `<img src="${country.flags.svg}" alt="Flag" width="30" height="24"></img>
                 <h2 class="country-info-title">${country.name.official}</h2>
             <p>Capital: <span>${country.capital}</span></p>
             <p>Population: <span>${country.population}</span></p>
-            <p>Languages: <span>${Object.values(country.languages)}</span></p>`
+            <p>Languages: <span>${Object.values(country.languages).join(', ')}</span></p>`
    })
     .join('');
     refsInfo.insertAdjacentHTML('beforeend', markup);

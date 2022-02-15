e91674ff39b0a2c697511de4851102bef6dc4d37
Vadym Starchenko <starvadim@ukr.net>
2022-02-15 08:30:54 +0200

    Update fetchCountries.js

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,6 +1,7 @@
 function fetchCountries(name) {
-  const URL = `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`;
-      return fetch(URL).then((response) => {
+  const URL = "https://restcountries.com/v3.1/name/";
+    return fetch(`${URL}${name}?fields=name,capital,population,flags,languages`)
+         .then((response) => {
             console.log(response);
       if (!response.ok) {
         throw new Error(response.message);
